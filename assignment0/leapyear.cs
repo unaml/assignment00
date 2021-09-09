a20fc6f91ae2adcc08c9181ce75f38939777f676
unalangeland <unla@itu.dk>
2021-09-09 21:39:21 +0200

    Created user interface for IsLeapYear

--- assignment0/leapyear.cs
+++ assignment0/leapyear.cs
@@ -7,25 +7,27 @@ namespace assignment0
     {
         public static bool IsLeapYear(int year)
         {
-
+            string val = Console.ReadLine();
+            int number = Convert.ToInt32(val);
+            year = number;
 
             if (year % 4 == 0)
             {
                 if (year % 400 == 0 || year % 100 != 0)
                 {
-                    //Console.WriteLine("yay");
+                    Console.WriteLine("yay");
                     return true;
 
                 }
                 else
                 {
-                    //Console.WriteLine("nay");
+                    Console.WriteLine("nay");
                     return false;
                 }
             }
             else
             {
-                //Console.WriteLine("nay");
+                Console.WriteLine("nay");
                 return false;
             }
             //return year % 4 == 0 && (year % 400 == 0 || year % 100 != 0);
